25decc478c22b815597b629290c7abd9516b5767
Sam0294 <sam_f94@hotmail.com>
2022-07-18 16:11:46 +0000

    ultimando

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -5,13 +5,13 @@ function Card(props) {
 	return (
 		<div className="container p-2">
 			<div className="clock d-flex container justify-content-center p-5">
-				<i className="far fa-clock"></i>
-				<div className="six p-2">{props.PossSix % 10}</div>
-				<div className="five p-2">{props.PossFive % 10}</div>
-				<div className="four p-2">{props.PossFour % 10}</div>
-				<div className="three p-2">{props.PossThree % 10}</div>
-				<div className="two p-2">{props.PossTwo % 10}</div>
-				<div className="one p-2">{props.PossOne % 10}</div>
+				<i className="far fa-clock p-2 border"></i>
+				<div className="six p-2 border">{props.PossSix % 10}</div>
+				<div className="five p-2 border">{props.PossFive % 10}</div>
+				<div className="four p-2 border">{props.PossFour % 10}</div>
+				<div className="three p-2 border">{props.PossThree % 10}</div>
+				<div className="two p-2 border">{props.PossTwo % 10}</div>
+				<div className="one p-2 border">{props.PossOne % 10}</div>
 			</div>
 		</div>
 	);
